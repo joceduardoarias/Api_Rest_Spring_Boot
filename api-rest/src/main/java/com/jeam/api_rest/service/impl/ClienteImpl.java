3448fdbf6f6a640a516bd6242f802b0f613f34b9
Jose Arias <josee.ariasmelo@gmail.com>
2024-11-12 12:32:40 -0300

    feat(service): Se agrega Transactional annotation
    - Envuelve el método anotado en una transacción. Si todo el método se ejecuta sin errores, la transacción se confirma (commit). Si ocurre alguna excepción, la transacción se revierte (rollback), evitando cambios parciales en la base de datos.

--- api-rest/src/main/java/com/jeam/api_rest/service/impl/ClienteImpl.java
+++ api-rest/src/main/java/com/jeam/api_rest/service/impl/ClienteImpl.java
@@ -3,6 +3,7 @@ package com.jeam.api_rest.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jeam.api_rest.model.entity.Cliente;
 import com.jeam.api_rest.model.entity.dao.ClienteDao;
@@ -16,11 +17,13 @@ public class ClienteImpl implements ICliente {
 	@Autowired
 	private ClienteDao clienteDao;
 	
+	@Transactional(readOnly = true) // Indicamos que es de solo lectura y no lanza excepción si falla.
 	@Override
 	public List<Cliente> findAll() {
 		return (List<Cliente>) clienteDao.findAll();
 	}
-
+	
+	@Transactional(readOnly = true)
 	@Override
 	public Cliente findById(Long id) {
 		return clienteDao.findById(id).orElse(null);
@@ -31,6 +34,7 @@ public class ClienteImpl implements ICliente {
 		return clienteDao.save(cliente);
 	}
 	
+	@Transactional // Indicamos que es una transacción de escritura y lectura. Lanza una excepción si falla.
 	@Override
 	public void delete(Long id) {
 		clienteDao.deleteById(id);
